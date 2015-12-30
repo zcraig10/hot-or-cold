93080eb3bbaec87bef204f3f3429ecaee8cbe9d1
Zack Craig <zack.e.craig@gmail.com>
2015-12-29 21:19:53 -0500

    finished

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 
+
+
 $(document).ready(function(){
 	
 	/*--- Display information modal box ---*/
@@ -12,28 +14,101 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+    
+    $input = $('#userGuess');
+
+
+
+    /*--- Start a new game ---*/
+    $("#newButton").click(function(){
+      newGame();
+    });
+
+    /*--- Take in the user's guess and store it in the guessList ---*/
+    $("#guessButton").click(function(){
+      var boxValue = parseInt($("#userGuess").val());
+      if (isValidGuess(boxValue)) {
+        feedback(boxValue);
+        increaseCount();
+        pastGuesses.push(boxValue);
+      }
+      afterGuess();
+    });
+
+    newGame();
 });
 
-/* 	function newGame(click) {
-  		var reload = window.location.reload();
-  		reload(onClick);
+  var pastGuesses = [];
+  var secretNumber = 0;
+  var numGuesses = 0;
+ 	function newGame() {
+  		secretNumber = getRandomNumber(0, 100);
+      numGuesses = 0; 
+      pastGuesses = [];
+      $('#userGuess').val('');
+      $('#count').text(numGuesses);
+      $('#feedback').text('Make your Guess!');
+      $('#guessList').text('');
+      $('#guessButton').prop('disabled', false);
   	}
 
-  	newGame(.new);
-
-  	function secretNumber(number){
-  		var randomNumber = Math.floor(Math.random()*101);
-  	}
-
-  	secretNumber(randomNumber); */
+  function getRandomNumber(min, max) { 
+      return Math.floor(Math.random() * (max - min)) + min; 
+    }
 
 
-  	/* Objectives:
 
-  	- make clicking new game start the game over
-  	- when the page loads, it generates a secret, random number from 0 to 100
-  	- when the user guesses a number in that range, it tells them if they are hot or cold
-  	- tell the user how many guesses they have taken and what numbers they have guessed
-    
-  	*/
+  function feedback(boxValue) {
+    console.log(boxValue);
+    console.log(secretNumber);
+    $('#guessList').append('<li>'+boxValue+'</li> ')
+      if (secretNumber == boxValue) {
+        winner();
+      }
+      else if (Math.abs(secretNumber - boxValue) < 10) {
+        $('#feedback').text("Hot");
+      }
+      else if (Math.abs(secretNumber - boxValue) < 20 && Math.abs(secretNumber - boxValue) > 9) {
+        $('#feedback').text('Kinda hot');
+      } 
+      else if (Math.abs(secretNumber - boxValue) < 30 && Math.abs(secretNumber - boxValue) > 19) {
+        $('#feedback').text('Less than warm');
+      }
+      else {
+        $('#feedback').text('Cold');
+      }
+  }
+
+  function winner() {
+    $('#feedback').text('You won! Click new game to play again')
+      $('#guessButton').prop('disabled', true);
+  }
+
+
+  function increaseCount() {
+    numGuesses++;
+    $('#count').text(numGuesses);
+  }
+
+  function afterGuess() {
+    $('#userGuess').val('');
+  }
+
+  function isValidGuess(boxValue) {
+      if(isNaN(boxValue)) {
+        alert('Please input a number');
+        return false;
+      }
+      if(boxValue < 0 || boxValue > 100) {
+        alert('Please choose a number between 0 and 100');
+        return false;
+      }
+      if(pastGuesses.indexOf(boxValue) > -1) {
+        alert('You already guessed this number');
+        return false;
+      } 
+
+      return true;
+    }
+
 
